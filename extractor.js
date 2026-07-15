39db10ac03ceab394c31c79db0c5adda035528a3
Kev1265 <Kevin.martineau.cissslan@ssss.gouv.qc.ca>
2026-07-15 09:46:24 -0400

    Update extractor.js

--- extractor.js
+++ extractor.js
@@ -67,9 +67,13 @@
   }
 
   async function setSelect(el, value, waitMs) {
+    el.focus();
+    el.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
     el.value = value;
-    el.dispatchEvent(new Event('change', { bubbles: true }));
     el.dispatchEvent(new Event('input', { bubbles: true }));
+    el.dispatchEvent(new Event('change', { bubbles: true }));
+    el.dispatchEvent(new MouseEvent('mouseup', { bubbles: true }));
+    el.dispatchEvent(new Event('blur', { bubbles: true }));
     await new Promise(r => setTimeout(r, waitMs));
   }
 
